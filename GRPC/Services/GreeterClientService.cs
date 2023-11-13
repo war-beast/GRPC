fdf41561480566dfb91602e90d1ffc5f8e493b73
Vladimir Sharapov <v.sharapov@artlogics.ru>
2023-11-13 17:59:34 +0300

    Заработал безопасный протокол

--- GRPC/Services/GreeterClientService.cs
+++ GRPC/Services/GreeterClientService.cs
@@ -1,19 +1,41 @@
-﻿using GRPC.Client.Interfaces;
+﻿using System.Net;
+using System.Security.Authentication;
+using GRPC.Client.Interfaces;
 using Grpc.Core;
 using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
 
 namespace GRPC.Client.Services;
 
 public class GreeterClientService : IGreeterClientService
 {
+	private readonly IHttpClientFactory _httpClientFactory;
+
+	public GreeterClientService(IHttpClientFactory httpClientFactory)
+	{
+		_httpClientFactory = httpClientFactory;
+	}
+
 	public async Task<string> CallGreeterMessage(string name, CancellationToken token)
 	{
+		//AppContext.SetSwitch("Microsoft.AspNetCore.Server.Kestrel.EnableWindows81Http2", true);
+		//HttpClient.DefaultProxy = new WebProxy();
+		//var httpClient = _httpClientFactory.CreateClient();
+		//httpClient.DefaultRequestVersion = new Version(2, 0);
+		//httpClient.DefaultVersionPolicy = HttpVersionPolicy.RequestVersionOrLower;
 
-		using var channel = GrpcChannel.ForAddress("http://localhost:5112");
+		//var handler = new HttpClientHandler();
+		//handler.ServerCertificateCustomValidationCallback =
+		//	HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+		var handler = new GrpcWebHandler(GrpcWebMode.GrpcWebText, new HttpClientHandler());
+		handler.HttpVersion = new Version(1, 1);
+
+		using var channel = GrpcChannel.ForAddress("https://localhost:7037", new GrpcChannelOptions{ HttpHandler = handler});
 
 		try
 		{
 			var client = new Greeter.GreeterClient(channel);
+
 			var reply = await client.SayHelloAsync(new HelloRequest { Name = name }, deadline: DateTime.MaxValue, cancellationToken: token);
 
 			return reply.Message;
--- SimpleGrpcService/Program.cs
+++ SimpleGrpcService/Program.cs
@@ -2,6 +2,14 @@ using Microsoft.AspNetCore.Server.Kestrel.Core;
 using SimpleGrpcService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.ConfigureKestrel(opt =>
+{
+	opt.ListenLocalhost(7037, o =>
+	{
+		o.Protocols = HttpProtocols.Http1AndHttp2;
+		o.UseHttps();
+	});
+});
 
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
@@ -11,8 +19,13 @@ builder.Services.AddGrpc();
 
 var app = builder.Build();
 
+app.UseGrpcWeb();
+
+app.UseHttpsRedirection();
+
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+app.MapGrpcService<GreeterService>()
+	.EnableGrpcWeb();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
